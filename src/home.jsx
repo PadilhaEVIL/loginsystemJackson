6c93a689f8b7aaf135133b6697eef81842c2c1f0
PadilhaEVIL <125047293+PadilhaEVIL@users.noreply.github.com>
2024-04-26 09:28:55 -0300

    Add files via upload

---
+++ src/home.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function Home() { 
+    return (
+      <div>
+        <h1>Bem Vindo </h1>
+        <h2>E ESCUTE O SOM DO JACKSON</h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/i6lYHgAIwOk?si=6hNR_tdlWf6bC-6O" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+    );
+  
+  }
+  
+  export  {Home};
